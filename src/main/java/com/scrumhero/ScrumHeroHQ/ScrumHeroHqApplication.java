ef69a4dd63632821ea06b7a6d0d3aa3518c1bd5a
Jam <milaperipolli@gmail.com>
2018-05-17 22:38:09 -0300

    First commit

---
+++ src/main/java/com/scrumhero/ScrumHeroHQ/ScrumHeroHqApplication.java
@@ -0,0 +1,12 @@
+package com.scrumhero.ScrumHeroHQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumHeroHqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumHeroHqApplication.class, args);
+	}
+}
---
+++ src/test/java/com/scrumhero/ScrumHeroHQ/ScrumHeroHqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scrumhero.ScrumHeroHQ;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScrumHeroHqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
