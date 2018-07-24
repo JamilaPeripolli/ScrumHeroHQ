6d22f1376cac42a1c1d5321497aa9ab739f671ac
JamilaPeripolli <32621357+JamilaPeripolli@users.noreply.github.com>
2018-07-23 22:52:44 -0300

    User entity, service and repository (#2)
    * Service to save and delete users.
    
    * Fixing method name in UserRepository, the method is used to find user by email and password

---
+++ src/main/java/com/scrumhero/scrumherohq/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.scrumhero.scrumherohq.repository;
+
+import com.scrumhero.scrumherohq.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmailAndPassword(String email, String password);
+}
